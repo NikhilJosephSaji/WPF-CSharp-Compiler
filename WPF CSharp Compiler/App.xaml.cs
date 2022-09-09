5293d8edd4832c3be0afa92c6a7ef8dc6126c006
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-09-09 13:02:18 +0530

    Code for Adding Environmental Variables from Program
    Code for Adding Environmental Variables from Program

--- WPF CSharp Compiler/App.xaml.cs
+++ WPF CSharp Compiler/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace WPF_CSharp_Compiler
@@ -9,7 +10,21 @@ namespace WPF_CSharp_Compiler
     /// </summary>
     public partial class App : Application
     {
+        private const string DOTNET = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319";
+
         private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                await BatchFileCreation();
+                var result = GetEnvironmentVariableValue("Path");
+                if (!result.Contains(DOTNET))
+                    SetEnvironmentVariableValue("Path", result + ";" + DOTNET);
+            }
+            catch (Exception) { }
+        }
+
+        private async Task BatchFileCreation()
         {
             var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             string filename = "\\Main.bat";
@@ -27,6 +42,23 @@ namespace WPF_CSharp_Compiler
             {
                 await File.WriteAllTextAsync(path + filename, content);
             }
+        }
+
+        public string GetEnvironmentVariableValue(string name, bool ExpandVariables = true)
+        {
+            if (ExpandVariables)
+            {
+                return Environment.GetEnvironmentVariable(name);
+            }
+            else
+            {
+                return (string)Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Environment\").GetValue(name, "", Microsoft.Win32.RegistryValueOptions.DoNotExpandEnvironmentNames);
+            }
+        }
+
+        public void SetEnvironmentVariableValue(string name, string value)
+        {
+            Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.Machine);
         }
     }
 }
